8df8c5c8c82eb0bdaf1b4a1945cffd874bf9a914
nemo-321 <alexagolubev24@gmail.com>
2024-12-03 16:05:38 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,20 @@
-name="name:Алексей"
-print(name)
-name="age:52"
-print(name)
-name="new age:53"
-print(name)
-name="Is Student:True"
-print(name)
+# Создание переменной name и присвоение ей значения
+name = "Алексей"
+# Вывод значения переменной name
+print("Name:", name)
+
+# Создание переменной age и присвоение ей значения
+age = 52
+# Вывод значения переменной age
+print("Age:", age)
+
+# Перезапись age, добавляя новое значение (например, 1)
+new_value = 1
+age += new_value
+# Вывод измененного значения переменной age
+print("New Age:", age)
+
+# Создание переменной is_student и присвоение ей значения True
+is_student = True
+# Вывод значения переменной is_student
+print("Is Student:", is_student)
